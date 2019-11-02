fc5db6d9b3b857c5d25b48b68b8b8e5f08d98415
nfrumkin <nfrumkin@bu.edu>
2019-11-02 11:42:49 -0400

    make g_loss an array

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from subgradient_descent import sgd
+import numpy as np
+
+k = 5
+def generate_data():
+    x = np.random.uniform(-1, 0, size = (100,k) )
+    y = np.sum(x**2, axis=1)
+    return x,y
 
 if __name__ == "__main__":
     loss = "l1"
     losses = []
     T = 100
     L = 1000
+
+    x_t = np.zeros( (1,k) )
+    z, y = generate_data()
     model = sgd(loss, L)
+
     for t in range(0,T):
-        loss_t, x_t = sgd.step(x_t, z_t, y_t)
+        z_t = z[t,:]
+        y_t = y[t]
+        loss_t, x_t = model.step(x_t, z_t, y_t)
         losses.append(loss_t)
+        print("x_t: ", x_t)
 
--- subgradient_descent.py
+++ subgradient_descent.py
@@ -1,4 +1,7 @@
+import numpy as np
+
 class sgd:
+    t = 0
     def __init__(self, loss='l1', L=1000):
         self.loss = loss
         self.L = L
@@ -6,15 +9,15 @@ class sgd:
 
     def step(self, x_t, z_t, y_t, eta=-1):
         if eta == -1:
-            eta = np.sqrt(this.t)
-        if this.loss == 'l1':
+            eta = np.sqrt(self.t)
+        if self.loss == 'l1':
             loss_t = self.l1_loss(x_t, z_t, y_t)
             g_loss = self.g_l1_loss(x_t, z_t, y_t)
         elif this.loss == 'l2':
             loss_t = self.l2_loss(x_t, z_t, y_t)
             g_loss = self.g_l2_loss(x_t, z_t, y_t)
         
-        x_new = greedy_projection(x_t - eta*g_loss)
+        x_new = self.greedy_projection(x_t - eta*g_loss)
         
         self.t = self.t + 1
         
@@ -28,8 +31,17 @@ class sgd:
 
     # gradient of l1 loss
     def g_l1_loss(self, x_t, z_t, y_t):
-        raise NotImplementedError("todo.")
+        g_loss = y_t - np.max(np.multiply(x_t, z_t))
+        g_loss = g_loss / np.absolute(y_t - np.max(np.multiply(x_t, z_t)))
+        k = self.max_affine_hyperplane(x_t, z_t)
+        
+        v = np.zeros(z_t.shape)
+        v[k] = z_t[k]
+        g_loss = np.multiply(g_loss, v)
+        return g_loss
     
+    def max_affine_hyperplane(self, a, x):
+        return np.argmax(np.multiply(a,x))
     # gradient of l2 loss
     def g_l2_loss(self, x_t, z_t, y_t):
         raise NotImplementedError("todo.")
@@ -38,6 +50,7 @@ class sgd:
     def greedy_projection(self, x):
         # projection based on the convex set of
         # max-affine functions where the norm is bounded
-        if np.absolute(x) > self.L:
-            x = self.L
+        for i in range(0,len(x)):
+            if np.absolute(x[:,i]) > self.L:
+                x[:,i] = self.L
         return x
